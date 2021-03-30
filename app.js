eeaca9c42a5ef453b673a09687ec09d33cd76874
Sonet Adhikary <sonet@pop-os.localdomain>
2021-03-30 11:54:47 +0600

    performance improved

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-document.addEventListener('keyup', (e) => {
+document.addEventListener('keydown', (e) => {
   const keyMatch = e.key.toUpperCase();
   const keyBoard = document.querySelectorAll('.container div');
 
   keyBoard.forEach((k) => {
     if (k.textContent == keyMatch) {
       let audio = new Audio(`./sounds/${keyMatch}.wav`);
+      audio.currentTime = 0;
       audio.play();
       k.className = 'playing';
     }
